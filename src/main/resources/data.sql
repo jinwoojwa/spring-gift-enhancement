fecfa6ad7dce79133437765e24de676ac121e703
jinwoojwa <jjw6654@gmail.com>
2025-07-14 14:17:14 +0900

    chore: 위시 리스트 코드 가져오기

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO product (name, price, image_url)
+VALUES ('Cold Brew', 5100, 'https://image.istarbucks.co.kr/upload/store/skuimg/2025/06/[9200000000038]_20250626095744579.jpg');
+
+INSERT INTO product (name, price, image_url)
+VALUES ('Java Chip Frappuccino', 6200, 'https://image.istarbucks.co.kr/upload/store/skuimg/2025/06/[168016]_20250626113601250.jpg');
+
+INSERT INTO product (name, price, image_url)
+VALUES ('Malcha Tiramisu Latte', 6300, 'https://image.istarbucks.co.kr/upload/store/skuimg/2023/11/[9200000004954]_20231127093740911.jpg');
+
+-- member 추가
+
+INSERT INTO member (email, password, role) VALUES
+('user1@example.com', 'password123', 'USER'),
+('admin@example.com', 'admin', 'ADMIN'),
+('user2@example.com', 'user2pass', 'USER');
