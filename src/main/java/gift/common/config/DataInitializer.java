ba8a8272c3197b40b57da3e4c6e77c0c73d6c00e
jinwoojwa <jjw6654@gmail.com>
2025-07-20 21:02:23 +0900

    feat: 데이터 초기화 로직에 옵션(Option) 초기 데이터 추가

--- src/main/java/gift/common/config/DataInitializer.java
+++ src/main/java/gift/common/config/DataInitializer.java
@@ -2,6 +2,8 @@ package gift.common.config;
 
 import gift.member.entity.Role;
 import gift.member.repository.MemberRepository;
+import gift.option.entity.Option;
+import gift.option.repository.OptionRepository;
 import gift.product.entity.Product;
 import gift.member.entity.Member;
 import gift.product.repository.ProductRepository;
@@ -13,19 +15,37 @@ public class DataInitializer implements CommandLineRunner {
 
     private final ProductRepository productRepository;
     private final MemberRepository memberRepository;
+    private final OptionRepository optionRepository;
 
     public DataInitializer(ProductRepository productRepository,
-                           MemberRepository memberRepository) {
+                           MemberRepository memberRepository,
+                           OptionRepository optionRepository) {
         this.productRepository = productRepository;
         this.memberRepository = memberRepository;
+        this.optionRepository = optionRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         // 제품 초기 데이터
-        productRepository.save(new Product("Cold Brew", 5100, "https://image.istarbucks.co.kr/upload/store/skuimg/2025/06/[9200000000038]_20250626095744579.jpg"));
-        productRepository.save(new Product("Frappuccino", 6200, "https://image.istarbucks.co.kr/upload/store/skuimg/2025/06/[168016]_20250626113601250.jpg"));
-        productRepository.save(new Product("Malcha Latte", 6300, "https://image.istarbucks.co.kr/upload/store/skuimg/2023/11/[9200000004954]_20231127093740911.jpg"));
+        Product coldBrew = new Product("콜드브루", 4500, "https://image.istarbucks.co.kr/upload/store/skuimg/2025/06/[9200000000038]_20250626095744579.jpg");
+        Product americano = new Product("아메리카노", 4000, "https://image.istarbucks.co.kr/upload/store/skuimg/2025/06/[110563]_20250626094354080.jpg");
+        Product cappuccino = new Product("카푸치노", 5000, "https://image.istarbucks.co.kr/upload/store/skuimg/2021/04/[38]_20210415154821991.jpg");
+
+        productRepository.save(coldBrew);
+        productRepository.save(americano);
+        productRepository.save(cappuccino);
+
+        // 옵션 초기 데이터
+        Option option1 = Option.of("Tall", 10, americano);
+        Option optton2 = Option.of("Grande", 5, americano);
+        Option option3 = Option.of("Tall", 3, coldBrew);
+        Option option4 = Option.of("Venti", 2, cappuccino);
+
+        optionRepository.save(option1);
+        optionRepository.save(optton2);
+        optionRepository.save(option3);
+        optionRepository.save(option4);
 
         // 회원 초기 데이터
         memberRepository.save(new Member("user1@example.com", "password1", Role.USER));
